6a383003473bb855432a0f8bb7e2069d40814dbd
Joel Ojerinde <joelojerinde@gmail.com>
2022-11-13 17:48:41 +0100

    feat: Adjust the html structure

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <div id="overlays"></div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
